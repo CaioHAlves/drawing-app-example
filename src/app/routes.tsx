ab28420a36c7ad5735de65a2045b1730f3a53c6e
CaioAlves <caio.alves@casadoconstrutor.com.br>
2023-07-26 17:08:15 -0300

    chore: atualização da biblioteca de desenho

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,29 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Container } from '../components/globalComponents';
-import { useHooks } from '../hooks/useHooks';
-import { useRedux } from '../hooks/useRedux';
-import { useValidations } from '../hooks/useValidations';
 import { InitialPage } from '../pages';
 
 const Rotas = () => {
-
-  const { isLoggedIn, logout } = useValidations()
-  const { useAppSelect } = useRedux()
-  const { history, local } = useHooks()
-
-  const { token, name } = useAppSelect.user
-  const dataDeExpiracao = Number(sessionStorage.getItem("data_exp")) > new Date().getTime()
-
-  const authenticated = isLoggedIn(token, name)
-
-  useEffect(() => {
-    authenticated && local.pathname === "/" && history.push("/home")
-
-    if (authenticated && dataDeExpiracao === false) {
-      logout()
-    }
-  }, [])
 
   return (
     <Container>
--- src/hooks/useRedux.ts
+++
@@ -1,9 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../redux/store";
-
-export function useRedux() {
-  return {
-    dispatch: useDispatch(),
-    useAppSelect: useSelector((state: RootState) => state),
-  }
-}
